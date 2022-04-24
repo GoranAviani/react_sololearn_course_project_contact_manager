42c148664175c4b52681ca9096cdf36153e8e6c1
GoranAviani <goranaviani@gmail.com>
2022-04-24 09:22:31 +0200

    react_sololearn_course_project_contact_manager

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
 
     return (
         <div>
-            <AddPersonForm></AddPersonForm>
+            <AddPersonForm/>
             <PeoplesList contacts={CONTACTS}/>
         </div>
     )
--- src/components/AddPersonForm.js
+++ src/components/AddPersonForm.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const AddPersonForm = () => {
-    return <div>Add person</div>
+    return (
+        <div>Add person</div>
+    )
 }
 
 export default AddPersonForm;
