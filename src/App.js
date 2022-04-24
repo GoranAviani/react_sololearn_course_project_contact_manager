11e500667582681f5d8ed1d79bfa809e43550126
GoranAviani <goranaviani@gmail.com>
2022-04-24 09:22:12 +0200

    react_sololearn_course_project_contact_manager

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import PeoplesList from "./components/PeoplesList";
+import AddPersonForm from "./components/AddPersonForm";
 
 const CONTACTS = ['person1', 'person2', 'person3']
 
@@ -8,6 +9,7 @@ const App = () => {
 
     return (
         <div>
+            <AddPersonForm></AddPersonForm>
             <PeoplesList contacts={CONTACTS}/>
         </div>
     )
